e041a75c21060b5edfcc1934219529e00969dd95
Shankar <admin@shankytech.com>
2020-11-08 21:28:26 -0500

    updated progress

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,7 +1,10 @@
+const Bootcamp = require ('../models/Bootcamp');
+
 //@desc     Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
 exports.getBootcamps = (req, res, next) => {
+    console.log(req.body);
     res.status(200).json({ success: true, msg: 'Show all bootcamps'});
 }
 
@@ -19,11 +22,14 @@ exports.getBootcamp = (req, res, next) => {
 //@desc     Create new bootcamp
 //@route    POST /api/v1/bootcamps
 //@access   Private
-exports.createBootcamps = (req, res, next) => {
-    res
-    .status(200)
-    .json({ success: true, msg: 'Create new bootcamps'});
-}
+exports.createBootcamps = async(req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body); 
+
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
+};
 
 
 //@desc     Update bootcamp
--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const dotevn = require('dotenv');
 const logger = require ('./middleware/logger');
 const morgan = require ('morgan');
+const colors = require ('colors');
+const connectDB = require ('./config/db');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
+
 
 //Load env vars
 dotevn.config({path: './config/config.env'});
 
+//connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//Body parser
+app.use(express.json());
 
 //Dev loggin middleware
 if (process.env.NODE_ENV === 'development'){
@@ -17,18 +27,21 @@ if (process.env.NODE_ENV === 'development'){
 }
 
 
-
-
-
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 
-
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    //close server & exit process
+    server.close(() => process.exit(1));
+});
